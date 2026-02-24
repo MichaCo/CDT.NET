7cfd75c7a8e3d317d40d3fb6922a4c9160f4a42a
MichaC <MichaCo@users.noreply.github.com>
2026-02-24 06:56:24 +0100

    progres

--- benchmark/CDT.Comparison.Benchmarks/Program.cs
+++ benchmark/CDT.Comparison.Benchmarks/Program.cs
@@ -4,4 +4,26 @@
 
 using BenchmarkDotNet.Running;
 
+var bench = new ComparisonBenchmarks();
+bench.Setup();
+
+static void Print(string label, int triangles) =>
+    Console.WriteLine($"  {label,-22}  {triangles,6:N0} triangles");
+
+const int lineWidth = 38;
+Console.WriteLine("Constrained Delaunay Triangulation");
+Console.WriteLine(new string('-', lineWidth));
+Print("CDT.NET", bench.CDT_CdtNet());
+Print("artem-ogre/CDT (C++)", bench.CDT_NativeCdt());
+Print("Spade (Rust)", bench.CDT_Spade());
+Print("CGAL (C++)", bench.CDT_Cgal());
+Print("Triangle.NET", bench.CDT_TriangleNet());
+Console.WriteLine(new string('-', lineWidth));
+
+Console.WriteLine("Conforming Delaunay Triangulation");
+Print("CDT.NET", bench.CfDT_CdtNet());
+Print("NTS", bench.CfDT_Nts());
+Print("Triangle.NET", bench.CfDT_TriangleNet());
+Console.WriteLine(new string('-', lineWidth));
+
 BenchmarkSwitcher.FromAssembly(typeof(ComparisonBenchmarks).Assembly).Run(args);
